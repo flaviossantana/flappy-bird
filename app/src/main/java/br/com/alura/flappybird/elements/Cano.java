165cdb9945a2059dd13cbc45c21940070267175e
flavio.santana <9090flavio@gmail.com>
2018-06-04 21:50:27 -0300

    Criando os canos superiores

--- app/src/main/java/br/com/alura/flappybird/elements/Cano.java
+++ app/src/main/java/br/com/alura/flappybird/elements/Cano.java
@@ -12,24 +12,43 @@ public class Cano {
     private static final float LARGURA_DO_CANO = 200;
     public static final Paint COR = Cores.getCorDoCano();
     private final float alturaDoCanoinferior;
+    private float alturaDoCanoSuperior;
     private Tela tela;
     private int posicao;
 
     public Cano(Tela tela, int posicao) {
         this.tela = tela;
         this.posicao = posicao;
-        alturaDoCanoinferior = tela.getAltura() - ALTURA_DO_CANO;
+        alturaDoCanoinferior = tela.getAltura() - ALTURA_DO_CANO - valorAleatorio();
+        alturaDoCanoSuperior = 0 + ALTURA_DO_CANO + valorAleatorio();
     }
 
     public void desenhaNo(Canvas canvas){
         desenhaCanoInferior(canvas);
+        desenhaCanoSuperior(canvas);
+    }
+
+    public int valorAleatorio(){
+        return (int) (Math.random() * 400);
     }
 
     private void desenhaCanoInferior(Canvas canvas) {
         canvas.drawRect(posicao, alturaDoCanoinferior, posicao + LARGURA_DO_CANO, tela.getAltura(), COR);
     }
 
+    private void desenhaCanoSuperior(Canvas canvas) {
+        canvas.drawRect(posicao, 0, posicao + LARGURA_DO_CANO, alturaDoCanoSuperior, COR);
+    }
+
     public void move() {
-        this.posicao -= 5;
+        this.posicao -= 3;
+    }
+
+    public boolean saiuDaTela() {
+        return posicao + LARGURA_DO_CANO < 0;
+    }
+
+    public int getPosicao() {
+        return posicao;
     }
 }
--- app/src/main/java/br/com/alura/flappybird/elements/Canos.java
+++ app/src/main/java/br/com/alura/flappybird/elements/Canos.java
@@ -3,7 +3,9 @@ package br.com.alura.flappybird.elements;
 import android.graphics.Canvas;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 import br.com.alura.flappybird.graphic.Tela;
 
@@ -12,13 +14,15 @@ public class Canos {
     public static final int QUANTIDADE_DE_CANOS = 5;
     public static final int DISTANCIA_ENTRE_CANOS = 400;
     private final List<Cano> canos = new ArrayList<>();
+    private Tela tela;
 
     int posicao = 400;
 
     public Canos(Tela tela) {
         for(int i = 0; i< QUANTIDADE_DE_CANOS; i++) {
             posicao += DISTANCIA_ENTRE_CANOS;
-            canos.add(new Cano(tela, posicao));
+            this.tela = tela;
+            canos.add(new Cano(this.tela, posicao));
         }
     }
 
@@ -29,8 +33,26 @@ public class Canos {
     }
 
     public void move() {
-        for (Cano cano : canos) {
+
+        ListIterator<Cano> iterator = canos.listIterator();
+
+        while (iterator.hasNext()){
+            Cano cano = iterator.next();
             cano.move();
+
+            if(cano.saiuDaTela()){
+                iterator.remove();
+                iterator.add(new Cano(tela, getMaximo() + DISTANCIA_ENTRE_CANOS));
+            }
         }
+
+    }
+
+    private int getMaximo() {
+        int maximo = 0;
+        for (Cano cano: canos) {
+            maximo = Math.max(cano.getPosicao(), maximo);
+        }
+        return maximo;
     }
 }
