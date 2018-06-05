e44890386f92cf5853b07d6fac0dfe2d626a753f
flavio.santana <9090flavio@gmail.com>
2018-06-05 20:08:11 -0300

    Tratando colisões

--- app/src/main/java/br/com/alura/flappybird/ui/MainActivity.java
+++ app/src/main/java/br/com/alura/flappybird/ui/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
         this.game = new Game(this);
         container.addView(game);
 
