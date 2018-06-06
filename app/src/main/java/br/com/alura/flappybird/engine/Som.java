e0a3e9f16b7f164759327c7da3292806ef218a03
flavio.santana <9090flavio@gmail.com>
2018-06-05 23:00:25 -0300

    Adicionando som ao jogo

--- app/src/main/java/br/com/alura/flappybird/engine/Som.java
+++ app/src/main/java/br/com/alura/flappybird/engine/Som.java
@@ -8,20 +8,26 @@ import br.com.alura.flappy_bird.R;
 
 public class Som {
 
-    private final SoundPool soundPool;
     public static int PULO;
     public static int COLISAO;
     public static int PONTOS;
+    private final Context context;
+    private final SoundPool soundPool;
 
 
     public Som(Context context) {
+        this.context = context;
         soundPool = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
-        PULO = soundPool.load(context, R.raw.pulo, 1);
-        COLISAO = soundPool.load(context, R.raw.colisao, 1);
-        PONTOS = soundPool.load(context, R.raw.pontos, 1);
+        PULO = loadSound(R.raw.pulo);
+        COLISAO = loadSound(R.raw.colisao);
+        PONTOS = loadSound(R.raw.pontos);
     }
 
-    public void toca(int som){
+    private int loadSound(int colisao) {
+        return soundPool.load(this.context, colisao, 1);
+    }
+
+    public void toca(int som) {
         soundPool.play(som, 1, 1, 1, 0, 1);
     }
 
